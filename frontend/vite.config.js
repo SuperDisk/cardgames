de0ede49bcdca7370b8ce0aecce01879f40880e8
Nick Faro <yux50000@hotmail.com>
2024-05-11 20:59:27 +0400

    Add kind of lame hint

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,8 @@ import parenscriptPlugin from './vite-parenscript';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    hmr: false
+  },
   plugins: [preact({prefreshEnabled: false}), parenscriptPlugin()],
 });
